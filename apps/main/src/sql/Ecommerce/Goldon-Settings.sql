bbe99f86d24286b2112d99564f0b0d59f83866f6
bahram <bahram.rajabiws@gmail.com>
2026-05-18 11:21:44 +0330

    remove unused sql

--- apps/main/src/sql/Ecommerce/Goldon-Settings.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO Settings([key], [value], [type], [createdAt], [updatedAt])
-SELECT N'SITE_NAME', 'ecommerce', 'string', getdate(), getdate()
-
-
-INSERT INTO Settings([key], [value], [type], [createdAt], [updatedAt])
-SELECT N'CUSTOMER_NAME', 'goldongallery', 'string', getdate(), getdate()
--- apps/main/src/sql/PeriodContentManagement/TiaraRah-Settings.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Settings([key], [value], [type], [createdAt], [updatedAt])
-SELECT N'SITE_NAME', 'PeriodContentManagement', 'string', getdate(), getdate()
-
-
-INSERT INTO Settings([key], [value], [type], [createdAt], [updatedAt])
-SELECT N'CUSTOMER_NAME', 'TiyaraRah', 'string', getdate(), getdate()
